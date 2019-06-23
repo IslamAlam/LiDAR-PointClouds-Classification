b5eae0f8409915a7eccef369be3275a57ef64bce
Islam Mansour <is3mansour@gmail.com>
2019-06-23 14:59:51 +0000

    add more papers and scripts

--- README.md
+++ README.md
@@ -14,11 +14,16 @@ Papers:
 1. [KIT Software and Datasets](http://www.ipf.kit.edu/code.php)
 2. [SEMANTIC 3D SCENE INTERPRETATION: A FRAMEWORK COMBINING OPTIMAL
 NEIGHBORHOOD SIZE SELECTION WITH RELEVANT FEATURES](https://www.isprs-ann-photogramm-remote-sens-spatial-inf-sci.net/II-3/181/2014/isprsannals-II-3-181-2014.pdf)
+3. [Classifying Single Photon LiDAR data using Machine Learning and Open Source tools](http://www.eurosdr.net/sites/default/files/images/inline/10_garcia-morales.pdf)
 
 
 
+Helpfull Repo:
 
-
+1. [ecolidar_knowledgebase](https://github.com/eEcoLiDAR/ecolidar_knowledgebase)
+2. [point-cloud-processing](https://github.com/rockestate/point-cloud-processing)
+3. [Risk_Detection_UI](https://github.com/HaroldMurcia/Risk_Detection_UI/)
+4. [Dissertation](https://github.com/NoemiRoecklinger/dissertation/blob/5e235617296910075af3444fc3edc3ea589c1843/4_CreateAllFeatures_subset1000.ipynb)
 
 
 
---
+++ inc/pdal_pipline.py
@@ -0,0 +1,188 @@
+from pathlib import Path
+import time
+import pandas as pd
+
+
+import json
+import pdal
+
+
+def las_2_df(las_path):
+    pipe_LASreader =\
+    {
+      "pipeline":[
+        {
+          "type":"readers.las",
+          "filename":las_file,
+          "use_eb_vlr": "true"
+        }
+      ]
+    }
+    # """%lasinput
+    #print(pipe_reader)
+
+    pipeline = pdal.Pipeline(json.dumps(pipe_LASreader))
+    pipeline.validate()
+    print(pipeline.validate())
+    %time n_points = pipeline.execute()
+    lidar_df = pd.DataFrame(pipeline.arrays[0])
+    print("Number of points in LiDAR:", n_points)
+    #print(lidar_df)
+    lidar_df.head()
+    
+    return lidar_df
+
+def las_add_features(las_path):
+    inputfile = str(las_path)
+    pipe_features =\
+    {
+      "pipeline":[
+        {
+          "type":"readers.las",
+          "filename":inputfile
+        },
+          {
+            "type":"filters.lof",
+            "minpts":20
+          },
+          {
+            "type":"filters.nndistance",
+            "k":8
+          },
+          {
+            "type":"filters.eigenvalues",
+            "knn":8
+          },
+          {
+            "type":"filters.estimaterank",
+            "knn":8,
+            "thresh":0.01
+          },
+          {
+            "type":"filters.normal",
+            "knn":8
+          },
+          {
+            "type":"filters.radialdensity",
+            "radius":2.0
+          },
+          {
+            "type":"filters.approximatecoplanar",
+            "knn":8,
+            "thresh1":25,
+            "thresh2":6
+          },
+          {
+            "type":"filters.smrf",
+            "scalar":1.2,
+            "slope":0.2,
+            "threshold":0.45,
+            "window":16.0
+          },
+          {
+            "type":"filters.hag"
+          },
+          {
+          "type":"filters.elm",
+          "cell":20.0,
+          "class":7,
+          "threshold":1.0
+          },
+          {
+            "type":"writers.las",
+            "filename":outputfile,
+            "minor_version": "4",
+            "extra_dims":"all"
+        }
+      ]
+    }
+
+    pipe_add_features =\
+    {
+      "pipeline":[
+        {
+          "type":"readers.las",
+          "filename":inputfile
+        },
+          {
+            "type":"filters.lof",
+            "minpts":20
+          },
+          {
+            "type":"filters.nndistance",
+            "k":8
+          },
+          {
+            "type":"filters.eigenvalues",
+            "knn":8
+          },
+          {
+            "type":"filters.estimaterank",
+            "knn":8,
+            "thresh":0.01
+          },
+          {
+            "type":"filters.normal",
+            "knn":8
+          },
+          {
+            "type":"filters.radialdensity",
+            "radius":2.0
+          },
+          {
+            "type":"filters.approximatecoplanar",
+            "knn":8,
+            "thresh1":25,
+            "thresh2":6
+          },
+          {
+            "type":"filters.smrf",
+            "scalar":1.2,
+            "slope":0.2,
+            "threshold":0.45,
+            "window":16.0
+          },
+          {
+            "type":"filters.hag"
+          },
+          {
+          "type":"filters.elm",
+          "cell":20.0,
+          "class":7,
+          "threshold":1.0
+          },
+          {
+              "type":"filters.python",
+              "script":"PCFeatures3D.py",
+              "function":"calcFeatureDescr",
+              "add_dimension":['Linearity', 'Planarity', 'Scattering','Omnivariance', 'Anisotropy',
+    'Eigentropy', 'Eigen_Sum', 'Curvature_Change'],
+              "module": "anything"
+
+          },
+          {
+            "type":"writers.las",
+            "filename":outputfile,
+            "extra_dims":"all"
+        }
+      ]
+    }
+
+    #pipeline = pdal.Pipeline(json.dumps(pipe_reader))
+    pipeline = pdal.Pipeline(json.dumps(pipe_features))
+    #pipeline = pdal.Pipeline(json.dumps(pipe_add_features))
+
+    pipeline.validate()
+    print(pipeline.validate())
+
+    #%time 
+    start_time = time.time()
+    n_points = pipeline.execute()
+    elapsed_time_fl = (time.time() - start_time)
+    print('Time taken ', elapsed_time_fl,' seconds')
+
+    print("Number of points in LiDAR:", n_points)
+
+    lidar_df = pd.DataFrame(pipeline.arrays[0])
+    print(lidar_df)
+    lidar_df.head()
